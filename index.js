99ad07a45fc4ea7e3ebe4d3c5d7a9829233b6faf
QingjieShen <122641381+QingjieShen@users.noreply.github.com>
2024-04-23 15:50:45 -0400

    adds fetch crypto function

--- index.js
+++ index.js
@@ -17,5 +17,15 @@ async function getRandomImage() {
     }
 }
 
-getRandomImage()
+async function getCrypto(id) {
+    try {
+        const res = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`)
+        const data = await res.json()
+        console.log(data)
+    } catch(err) {
+        console.log(err)
+    }
+}
 
+getRandomImage()
+getCrypto("dogecoin")
