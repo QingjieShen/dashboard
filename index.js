56d78717c7d90f9f6797585b68767e846a1a635b
QingjieShen <122641381+QingjieShen@users.noreply.github.com>
2024-04-23 01:33:17 -0400

    adds author info and style

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+const author = document.getElementById('author')
+
 
 async function getRandomImage() {
     // const res = await fetch('https://api.unsplash.com/photos/random?orientation=landscape&query=nature?client_id=S_t6lYdKmSMtrIQaj72iYibQxW3qOSRhp-w_NcMZLXk')
     const res = await fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
     const data = await res.json()
     console.log(data)
-    console.log(data.links.download)
-    document.body.style.backgroundImage = `url(${data.links.download})`
+    console.log(data.urls.full)
+    document.body.style.backgroundImage = `url(${data.urls.full})`
+    author.textContent = `Photo By: ${data.user.name}`
 }
 
 getRandomImage()
